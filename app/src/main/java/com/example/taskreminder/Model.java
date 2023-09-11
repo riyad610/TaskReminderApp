1535c12353590d4302156748543132e14ca64e38
riyad610 <106856554+riyad610@users.noreply.github.com>
2023-09-11 19:28:57 +0530

    Added src

---
+++ app/src/main/java/com/example/taskreminder/Model.java
@@ -0,0 +1,42 @@
+package com.example.taskreminder;
+
+public class Model {
+    String title, date, time, category;
+
+    public Model() {
+    }
+
+    public Model(String title, String date, String time) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
